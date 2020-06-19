4172aea166a464647e5ebb33b0c643743e9402e7
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-06-18 22:47:25 -0400

    added slack post parsing

--- slackbot/slackbotRoutes.js
+++ slackbot/slackbotRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const db = require('../data/dbaccess')
+const parser = require('body-parser')
+
+router.use(parser.urlencoded({ extended: true }))
 
 router.post('/test', (req, res) => {
   console.log(req.body)
